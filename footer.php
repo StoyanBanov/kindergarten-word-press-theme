f7a01201d8b93ba4286c584108ceea85cec33bfe
StoyanBanov <sbanov05@gmail.com>
2023-05-15 22:09:44 +0300

    init

---
+++ footer.php
@@ -0,0 +1,135 @@
+<!-- info section -->
+<section class="info_section layout_padding">
+    <div class="container">
+        <div class="info_form">
+
+            <div class="row">
+                <div class="offset-lg-3 col-lg-3">
+                    <h5 class="form_heading">
+                        Newsletter
+                    </h5>
+                </div>
+                <div class="col-md-6">
+                    <form action="#">
+                        <input type="text" placeholder="Enter Your email">
+                        <button>
+                            subscribe
+                        </button>
+                    </form>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="info_logo">
+                    <div>
+                        <a href="">
+                            <img src="images/logo.png" alt="" />
+                            <span>
+                                Brighton
+                            </span>
+                        </a>
+                    </div>
+                    <p>
+                        There are many variations of passages of Lorem Ipsum available,
+                        but the majority have suffered alteration
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="info_links ">
+                    <h5>
+                        Contact Us
+                    </h5>
+                    <p class="pr-0 pr-md-4 pr-lg-5">
+                        Donec odio. Quisque volutpat mattis eros.Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec
+                        odio. Quisque volutpat mattis eros
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="info_insta">
+                    <h5>
+                        INFORMATION
+                    </h5>
+                    <p class="pr-0 pr-md-4 pr-md-5">
+                        Donec odio. Quisque volutpat mattis eros.Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec
+                        odio. Quisque volutpat mattis eros
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="pl-0 pl-lg-5 pl-md-4">
+                    <h5>
+                        MY ACCOUNT
+
+                    </h5>
+                    <p>
+                        Donec odio. Quisque volutpat mattis eros.Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec
+                        odio. Quisque volutpat mattis eros
+                    </p>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- end info_section -->
+
+<!-- footer section -->
+<section class="container-fluid footer_section">
+    <p>
+        &copy; 2019 All Rights Reserved By
+        <a href="https://html.design/">Free Html Templates</a>
+    </p>
+</section>
+<!-- footer section -->
+
+<script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+
+<script>
+    // This example adds a marker to indicate the position of Bondi Beach in Sydney,
+    // Australia.
+    function initMap() {
+        var map = new google.maps.Map(document.getElementById("map"), {
+            zoom: 11,
+            center: {
+                lat: 40.645037,
+                lng: -73.880224
+            }
+        });
+
+        var image = "images/maps-and-flags.png";
+        var beachMarker = new google.maps.Marker({
+            position: {
+                lat: 40.645037,
+                lng: -73.880224
+            },
+            map: map,
+            icon: image
+        });
+    }
+</script>
+<!-- google map js -->
+
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA8eaHt9Dh5H57Zh0xVTqxVdBFCvFMqFjQ&callback=initMap">
+</script>
+<!-- end google map js -->
+
+<script>
+    function openNav() {
+        document.getElementById("myNav").style.width = "100%";
+    }
+
+    function closeNav() {
+        document.getElementById("myNav").style.width = "0%";
+    }
+</script>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Basic -->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <!-- Site Metas -->
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+
+    <title>Brighton</title>
+
+    <!-- slider stylesheet -->
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.1.3/assets/owl.carousel.min.css" />
+
+    <!-- bootstrap core css -->
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/css/bootstrap.css" />
+
+    <!-- fonts style -->
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700|Poppins:400,700|Roboto:400,700&display=swap" rel="stylesheet" />
+
+    <!-- Custom styles for this template -->
+    <link href="<?php bloginfo('template_directory'); ?>/css/style.css" rel="stylesheet" />
+    <!-- responsive style -->
+    <link href="<?php bloginfo('template_directory'); ?>/css/responsive.css" rel="stylesheet" />
+</head>
+
+<body>
+    <div class="hero_area">
+        <!-- header section strats -->
+        <header class="header_section">
+            <div class="container">
+                <nav class="navbar navbar-expand-lg custom_nav-container ">
+                    <a class="navbar-brand" href="index.html">
+                        <img src="images/logo.png" alt="" />
+                        <span>
+                            Brighton
+                        </span>
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <div class="d-flex ml-auto flex-column flex-lg-row align-items-center">
+                            <ul class="navbar-nav  ">
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="index.html">
+                                        Home <span class="sr-only">(current)</span></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="about.html"> About </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="program.html"> Programs </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="contact.html"> Contact us</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </header>
+        <!-- end header section -->
